10d3bfd82305fe10f60961328096d5e7557f50e5
Дарья Щеколдина <darasekoldina@MacBook-Air-Dara.local>
2019-04-10 19:10:10 +0300

    release v1

---
+++ TextShooter/SKNode+Extra.swift
@@ -0,0 +1,12 @@
+
+import SpriteKit
+
+extension SKNode {
+    func receiveAttacker(_ attacker: SKNode, contact: SKPhysicsContact) {
+        // Default implementation does nothing
+    }
+    
+    func friendlyBumpFrom(_ node: SKNode) {
+        // Default implementation does nothing
+    }
+}
